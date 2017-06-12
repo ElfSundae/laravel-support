1ecfb4bb3360b3528c3c5a77dbb37f697f8441ae
Elf Sundae <elf.sundae@gmail.com>
2017-06-13 03:11:35 +0800

    Add fetch() helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use GuzzleHttp\Client as HttpClient;
+
 if (! function_exists('urlsafe_base64_encode')) {
     /**
      * Encodes the given data with base64, and returns an URL-safe string.
@@ -63,6 +65,78 @@ if (! function_exists('string_value')) {
         }
 
         return is_string($value) ? $value : json_encode($value, $jsonOptions | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    }
+}
+
+if (! function_exists('http_client')) {
+    /**
+     * Create a Guzzle http client.
+     *
+     * @param  array  $config
+     * @return \GuzzleHttp\Client
+     */
+    function http_client($config = [])
+    {
+        return new HttpClient(
+            array_merge([
+                'connect_timeout' => 5,
+                'timeout' => 25,
+            ], $config)
+        );
+    }
+}
+
+if (! function_exists('fetch')) {
+    /**
+     * Request an URL.
+     *
+     * @param  string  $url
+     * @param  string  $method
+     * @param  array   $options
+     * @return GuzzleHttp\Psr7\Response|null
+     */
+    function fetch($url, $method = 'GET', $options = [])
+    {
+        try {
+            return http_client()->request($method, $url, $options);
+        } catch (Exception $e) {
+        }
+    }
+}
+
+if (! function_exists('fetch_content')) {
+    /**
+     * Request an URL, and return the content.
+     *
+     * @param  string  $url
+     * @param  string  $method
+     * @param  array   $options
+     * @param  GuzzleHttp\Psr7\Response|null  &$response
+     * @return string
+     */
+    function fetch_content($url, $method = 'GET', $options = [], &$response = null)
+    {
+        if ($response = fetch($url, $method, $options)) {
+            return (string) $response->getBody();
+        }
+    }
+}
+
+if (! function_exists('fetch_json')) {
+    /**
+     * Request an URL, and return the JSON data.
+     *
+     * @param  string  $url
+     * @param  string  $method
+     * @param  array   $options
+     * @param  GuzzleHttp\Psr7\Response|null  &$response
+     * @return mixed
+     */
+    function fetch_json($url, $method = 'GET', $options = [], &$response = null)
+    {
+        array_set($options, 'headers.Accept', 'application/json');
+
+        return json_decode(fetch_content($url, $method, $options, $response), true);
     }
 }
 
