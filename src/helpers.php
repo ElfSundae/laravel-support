7b2c3861420c846eec27de9aebf9a911b142c126
Elf Sundae <elf.sundae@gmail.com>
2017-06-13 02:41:21 +0800

    Add request() helper

--- src/helpers.php
+++ src/helpers.php
@@ -66,6 +66,28 @@ if (! function_exists('string_value')) {
     }
 }
 
+if (! function_exists('request')) {
+    /**
+     * Get an instance of the current request or an input item from the request.
+     *
+     * @param  array|string  $key
+     * @param  mixed   $default
+     * @return \Illuminate\Http\Request|string|array
+     */
+    function request($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return app('request');
+        }
+
+        if (is_array($key)) {
+            return app('request')->only($key);
+        }
+
+        return data_get(app('request')->all(), $key, $default);
+    }
+}
+
 if (! function_exists('active')) {
     /**
      * Returns string 'active' if the current request URI matches the given patterns.
