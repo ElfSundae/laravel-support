a3b85df02c678067a24c09c47d219f6332b30d71
Elf Sundae <elf.sundae@gmail.com>
2017-06-22 07:05:27 +0800

    Move Optimus helper functions to Services/Optimus directory

--- src/helpers.php
+++ src/helpers.php
@@ -123,32 +123,6 @@ if (! function_exists('gravatar')) {
     }
 }
 
-if (! function_exists('optimus_encode')) {
-    /**
-     * Encode a number with Optimus.
-     *
-     * @param  int  $number
-     * @return int
-     */
-    function optimus_encode($number)
-    {
-        return app('optimus')->encode($number);
-    }
-}
-
-if (! function_exists('optimus_decode')) {
-    /**
-     * Decode a number with Optimus.
-     *
-     * @param  int  $number
-     * @return int
-     */
-    function optimus_decode($number)
-    {
-        return app('optimus')->decode($number);
-    }
-}
-
 if (! function_exists('random_uuid')) {
     /**
      * Generate a version 4 (random) UUID.
