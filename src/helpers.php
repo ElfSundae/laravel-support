ba51f41b54e6e699754c04e3c3575212cdf721f0
Elf Sundae <elf.sundae@gmail.com>
2017-06-23 02:20:02 +0800

    Methods order

--- src/helpers.php
+++ src/helpers.php
@@ -2,6 +2,68 @@
 
 use Ramsey\Uuid\Uuid;
 
+if (! function_exists('revision')) {
+    /**
+     * Get the revisioned asset path.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function revision($path)
+    {
+        if ($rev = array_get(config('assets-version'), trim($path, DIRECTORY_SEPARATOR))) {
+            return $path.'?'.$rev;
+        }
+
+        return $path;
+    }
+}
+
+if (! function_exists('random_uuid')) {
+    /**
+     * Generate a version 4 (random) UUID.
+     *
+     * @param  bool  $hex
+     * @return string
+     */
+    function random_uuid($hex = false)
+    {
+        $uuid = Uuid::uuid4();
+
+        return $hex ? $uuid->getHex() : $uuid->toString();
+    }
+}
+
+if (! function_exists('gravatar')) {
+    /**
+     * Generate a Gravatar url.
+     *
+     * @see http://cn.gravatar.com/site/implement/images/
+     *
+     * @param  string  $email
+     * @param  int  $size
+     * @param  string  $default
+     * @param  string  $rating
+     * @return string
+     */
+    function gravatar($email, $size = 100, $default = null, $rating = null)
+    {
+        if (is_null($default)) {
+            $default = config('support.gravatar.default');
+        }
+        if (is_null($rating)) {
+            $rating = config('support.gravatar.rating');
+        }
+
+        $query = http_build_query(array_filter(compact('size', 'default', 'rating')));
+
+        return app('url')->assetFrom(
+            config('support.gravatar.host', 'http://gravatar.com/avatar'),
+            md5(strtolower(trim($email))).'?'.$query
+        );
+    }
+}
+
 if (! function_exists('is_app')) {
     /**
      * Check the current application identifier.
@@ -44,23 +106,6 @@ if (! function_exists('app_url')) {
     }
 }
 
-if (! function_exists('revision')) {
-    /**
-     * Get the revisioned asset path.
-     *
-     * @param  string  $path
-     * @return string
-     */
-    function revision($path)
-    {
-        if ($rev = array_get(config('assets-version'), trim($path, DIRECTORY_SEPARATOR))) {
-            return $path.'?'.$rev;
-        }
-
-        return $path;
-    }
-}
-
 if (! function_exists('asset_url')) {
     /**
      * Generate an asset URL.
@@ -88,50 +133,5 @@ if (! function_exists('cdn_url')) {
     function cdn_url($path)
     {
         return asset_url($path, 'cdn');
-    }
-}
-
-if (! function_exists('gravatar')) {
-    /**
-     * Generate a Gravatar url.
-     *
-     * @see http://cn.gravatar.com/site/implement/images/
-     *
-     * @param  string  $email
-     * @param  int  $size
-     * @param  string  $default
-     * @param  string  $rating
-     * @return string
-     */
-    function gravatar($email, $size = 100, $default = null, $rating = null)
-    {
-        if (is_null($default)) {
-            $default = config('support.gravatar.default');
-        }
-        if (is_null($rating)) {
-            $rating = config('support.gravatar.rating');
-        }
-
-        $query = http_build_query(array_filter(compact('size', 'default', 'rating')));
-
-        return app('url')->assetFrom(
-            config('support.gravatar.host', 'http://gravatar.com/avatar'),
-            md5(strtolower(trim($email))).'?'.$query
-        );
-    }
-}
-
-if (! function_exists('random_uuid')) {
-    /**
-     * Generate a version 4 (random) UUID.
-     *
-     * @param  bool  $hex
-     * @return string
-     */
-    function random_uuid($hex = false)
-    {
-        $uuid = Uuid::uuid4();
-
-        return $hex ? $uuid->getHex() : $uuid->toString();
     }
 }
