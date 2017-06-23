334c7d0d4f7a6230a24412bbc7da489f2cfd76b6
Elf Sundae <elf.sundae@gmail.com>
2017-06-24 01:19:58 +0800

    code comments

--- config/support.php
+++ config/support.php
@@ -106,14 +106,13 @@ return [
     | Google Analytics
     |--------------------------------------------------------------------------
     |
-    | https://www.google.com/analytics
+    | id : 'UA-xxxxxxxx-x'
+    | cookie_domain : 'auto' // 'none' // 'example.com'
     |
     */
 
     'google_analytics' => [
-        // 'UA-xxxxxxxx-x'
         'id' => env('GOOGLE_ANALYTICS_ID'),
-        // 'auto', 'none', 'example.com'
         'cookie_domain' => env('GOOGLE_ANALYTICS_COOKIE_DOMAIN', 'auto'),
     ],
 
