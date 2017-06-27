a98ee9071c2e1257cb743ab39a617196d38d1fa3
Elf Sundae <elf.sundae@gmail.com>
2017-06-28 04:12:38 +0800

    public

--- src/Traits/AssetHelper.php
+++ src/Traits/AssetHelper.php
@@ -12,7 +12,7 @@ trait AssetHelper
      * @param  string|null  $identifier
      * @return \Illuminate\Contracts\Filesystem\Filesystem
      */
-    protected function getFilesystem($identifier = null)
+    public function getFilesystem($identifier = null)
     {
         return Storage::disk($this->getFilesystemDisk($identifier));
     }
