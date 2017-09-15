81c6f51661756efd799ee5cc1ed7561e24e6a1f1
Elf Sundae <elf.sundae@gmail.com>
2017-09-16 01:53:33 +0800

    Add 'captcha.default' config

--- config/support.php
+++ config/support.php
@@ -83,6 +83,15 @@ return [
                 'default' => 'identicon',
                 'rating' => 'pg',
             ],
+
+            'captcha.default' => [
+                'length' => 4,
+                'width' => 80,
+                'height' => 32,
+                'lines' => 0,
+                'bgImage' => true,
+                'fontColors' => ['#2c3e50', '#c0392b', '#16a085', '#c0392b', '#8e44ad', '#303f9f', '#f57c00', '#795548'],
+            ],
         ],
 
         'admin' => [
