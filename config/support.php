4bc664c863751dde4cb1a93b02e5d6f9c87dce4a
Elf Sundae <elf.sundae@gmail.com>
2017-11-12 16:20:53 +0800

    Remove Optimus service

--- config/support.php
+++ config/support.php
@@ -155,27 +155,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Optimus ID Transformation
-    |--------------------------------------------------------------------------
-    |
-    | More info: https://github.com/jenssegers/optimus#usage
-    |
-    | - Large prime number lower than 2147483647
-    | - The inverse prime so that (PRIME * INVERSE) & MAXID == 1
-    | - A large random integer lower than 2147483647
-    |
-    | You may generate them using `php artisan support:generate-optimus` command.
-    |
-    */
-
-    'optimus' => [
-        'prime' => env('OPTIMUS_PRIME'),
-        'inverse' => env('OPTIMUS_INVERSE'),
-        'random' => env('OPTIMUS_RANDOM'),
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
     | Google Analytics
     |--------------------------------------------------------------------------
     |
