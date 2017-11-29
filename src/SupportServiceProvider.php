aac8745434def5157c30e92b4293d2cfb2e6fde2
Elf Sundae <elf.sundae@gmail.com>
2017-11-30 00:28:36 +0800

    CS

--- src/SupportServiceProvider.php
+++ src/SupportServiceProvider.php
@@ -22,10 +22,9 @@ class SupportServiceProvider extends ServiceProvider
                 __DIR__.'/../config/support.php' => config_path('support.php'),
             ], 'laravel-support-config');
 
-            $this->publishes(
-                [__DIR__.'/../resources/lang' => resource_path('lang')],
-                'laravel-support-lang'
-            );
+            $this->publishes([
+                __DIR__.'/../resources/lang' => resource_path('lang'),
+            ], 'laravel-support-lang');
 
             $this->publishes([
                 __DIR__.'/../resources/views' => resource_path('views/vendor/support'),
