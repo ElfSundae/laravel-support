7885ae85f6c599b0d6edd36121c93ba837ceafb6
Elf Sundae <elf.sundae@gmail.com>
2017-12-11 22:34:55 +0800

    Remove support config file

--- src/SupportServiceProvider.php
+++ src/SupportServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace ElfSundae\Laravel\Support;
 
-use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class SupportServiceProvider extends ServiceProvider
@@ -18,14 +17,6 @@ class SupportServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/support.php' => config_path('support.php'),
-            ], 'laravel-support-config');
-
-            $this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang'),
-            ], 'laravel-support-lang');
-
-            $this->publishes([
                 __DIR__.'/../resources/views' => resource_path('views/vendor/support'),
             ], 'laravel-support-views');
         }
@@ -38,46 +29,10 @@ class SupportServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/support.php', 'support');
-
-        $this->setupConfiguration();
-
         if ($this->app->runningInConsole()) {
             $this->commands([
                 Console\IdeHelperGenerateCommand::class,
             ]);
-        }
-    }
-
-    /**
-     * Setup application configurations.
-     *
-     * @return void
-     */
-    protected function setupConfiguration()
-    {
-        if (! $this->app->configurationIsCached()) {
-            if ($defaults = $this->app['config']['support.config.default']) {
-                $this->app['config']->set($defaults);
-            }
-        }
-
-        $this->configureForCurrentRequest();
-    }
-
-    /**
-     * Configure application for the current request.
-     *
-     * @return void
-     */
-    protected function configureForCurrentRequest()
-    {
-        // if (app_id() && $appConfig = $this->app['config']->get('support.config.'.app_id())) {
-        //     $this->app['config']->set($appConfig);
-        // }
-
-        if ($carbonLocale = $this->app['config']['support.carbon_locale']) {
-            Carbon::setLocale($carbonLocale);
         }
     }
 }
