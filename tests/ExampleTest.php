206c0b38405795b4c572427e8aa6f4fca96ce39f
Elf Sundae <elf.sundae@gmail.com>
2017-12-12 00:55:06 +0800

    tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,8 +2,6 @@
 
 namespace ElfSundae\Laravel\Support\Test;
 
-use PHPUnit\Framework\TestCase;
-
 class ExampleTest extends TestCase
 {
     public function testExample()
---
+++ tests/TestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ElfSundae\Laravel\Support\Test;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+}
