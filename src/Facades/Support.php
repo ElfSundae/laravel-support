767580a96fbe915aa51964c37eb1f7ee48a2a20f
Elf Sundae <elf.sundae@gmail.com>
2017-12-27 03:02:51 +0800

    Add phpdoc for Support facade

--- src/Facades/Support.php
+++ src/Facades/Support.php
@@ -5,6 +5,13 @@ namespace ElfSundae\Support\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static string|null extension(mixed $file, string $prefix = '')
+ * @method static string|null extensionForMime(string $mimeType)
+ * @method static string iDeviceModel(string $platform)
+ * @method static string|null emailHome(string $email)
+ * @method static void fakeAppClient(array $client)
+ * @method static void fakeApiToken(string $appKey = null)
+ *
  * @see \ElfSundae\Support\Support
  */
 class Support extends Facade
